6726704194f3739e4e83f184f7beeb6d451acfba
daniel salinas jaramillo <daniel salinas jaramillo>
2016-05-02 20:37:18 -0500

    Initial commit

---
+++ app/src/main/java/danielsalinasjaramillo/com/turismocolombiav3/BaresFragment.java
@@ -0,0 +1,82 @@
+package danielsalinasjaramillo.com.turismocolombiav3;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.design.widget.TabLayout;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BaresFragment extends Fragment {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    public BaresFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_menu, container, false);
+        tabLayout = (TabLayout) view.findViewById(R.id.tabs);
+        viewPager = (ViewPager) view.findViewById(R.id.pager);
+        viewPager.setAdapter(new MyAdapter(getChildFragmentManager()));
+
+        tabLayout.post(new Runnable() {
+
+            @Override
+            public void run() {
+                tabLayout.setupWithViewPager(viewPager);
+            }
+        });
+
+        return view;
+    }
+
+    class MyAdapter extends FragmentPagerAdapter{
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0: return new FlashFragment();
+                case 1: return new SupermanFragment();
+                case 2: return new BatmanFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Flash";
+                case 1:
+                    return "Superman";
+                case 2:
+                    return "Batman";
+            }
+            return null;
+        }
+    }
+}
